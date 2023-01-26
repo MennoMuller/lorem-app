615e7798c7fa84d6cb36e0c711451513fe483d1b
MennoMuller <menno.muller.nl@gmail.com>
2023-01-26 14:16:40 +0100

    Made menus functional

--- src/components/ModifySiteMenu.js
+++ src/components/ModifySiteMenu.js
@@ -1,75 +1,70 @@
 import React from "react";
 
-class ModifySiteMenu extends React.Component {
-  render() {
-    return (
-      <div className="grid-item popup-window">
-        <h2>
-          {this.props.modify
-            ? "Modify website"
-            : "Add a website"}
-        </h2>
-        <form>
-          <div className="form-grid">
-            <div className="form-field site-name">
-              <label htmlFor="name">
-                Name website <span className="red">*</span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={this.props.name}
-              />
-            </div>
-            <div className="form-field site-url">
-              <label htmlFor="url">
-                Website URL <span className="red">*</span>
-              </label>
-              <input
-                type="text"
-                id="url"
-                name="url"
-                value={this.props.url}
-              />
-            </div>
-            <div className="form-field site-icon-field">
-              <label htmlFor="icon">Website icon</label>
-              <input
-                type="file"
-                id="icon"
-                name="icon"
-                // value={this.props.icon}
-              />
-            </div>
-            <div className="form-field site-description-field">
-              <label htmlFor="description">
-                Website description{" "}
-                <span className="red">*</span>
-              </label>
-              <input
-                type="text"
-                id="description"
-                name="description"
-                value={this.props.description}
-              />
-            </div>
+const ModifySiteMenu = (props) => {
+  return (
+    <div className="grid-item popup-window">
+      <h2>
+        {props.modify ? "Modify website" : "Add a website"}
+      </h2>
+      <form>
+        <div className="form-grid">
+          <div className="form-field site-name">
+            <label htmlFor="name">
+              Name website <span className="red">*</span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={props.name}
+            />
           </div>
-          <p>
-            <span className="red">*</span> mandatory field
-          </p>
-          <input
-            className="submit-button"
-            type="submit"
-            value={
-              this.props.modify
-                ? "Save changes"
-                : "Add website"
-            }
-          />
-        </form>
-      </div>
-    );
-  }
-}
+          <div className="form-field site-url">
+            <label htmlFor="url">
+              Website URL <span className="red">*</span>
+            </label>
+            <input
+              type="text"
+              id="url"
+              name="url"
+              value={props.url}
+            />
+          </div>
+          <div className="form-field site-icon-field">
+            <label htmlFor="icon">Website icon</label>
+            <input
+              type="file"
+              id="icon"
+              name="icon"
+              // value={this.props.icon}
+            />
+          </div>
+          <div className="form-field site-description-field">
+            <label htmlFor="description">
+              Website description{" "}
+              <span className="red">*</span>
+            </label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={props.description}
+            />
+          </div>
+        </div>
+        <p>
+          <span className="red">*</span> mandatory field
+        </p>
+        <input
+          className="submit-button"
+          type="submit"
+          value={
+            props.modify ? "Save changes" : "Add website"
+          }
+        />
+      </form>
+    </div>
+  );
+};
+
 export default ModifySiteMenu;
--- src/components/ModifyTaskMenu.js
+++ src/components/ModifyTaskMenu.js
@@ -1,97 +1,92 @@
 import React from "react";
 
-class ModifyTaskMenu extends React.Component {
-  render() {
-    return (
-      <div className="grid-item popup-window">
-        <h2>
-          {this.props.modify ? "Modify task" : "Add a task"}
-        </h2>
-        <form>
-          <div className="form-grid">
-            <div className="form-field task-name">
-              <label htmlFor="name">
-                Name task <span className="red">*</span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={this.props.name}
-              />
-            </div>
-            <div className="form-field task-category">
-              <label htmlFor="category">
-                Category <span className="red">*</span>
-              </label>
-              <select
-                id="category"
-                name="category"
-              >
-                <option
-                  value="School"
-                  selected={this.props.category == "School"}
-                >
-                  School
-                </option>
-                <option
-                  value="Social"
-                  selected={this.props.category == "Social"}
-                >
-                  Social
-                </option>
-                <option
-                  value="Home"
-                  selected={this.props.category == "Home"}
-                >
-                  Home
-                </option>
-                <option
-                  value="Misc"
-                  selected={this.props.category == "Misc"}
-                >
-                  Misc
-                </option>
-              </select>
-            </div>
-            <div className="form-field task-deadline-date">
-              <label htmlFor="deadline_date">
-                Deadline date
-              </label>
-              <input
-                type="date"
-                id="deadline_date"
-                name="deadline_date"
-                value={this.props.deadline_date}
-              />
-            </div>
-            <div className="form-field task-deadline-time">
-              <label htmlFor="deadline_time">
-                Deadline time
-              </label>
-              <input
-                type="time"
-                id="deadline_time"
-                name="deadline_time"
-                value={this.props.deadline_time}
-              />
-            </div>
+const ModifyTaskMenu = (props) => {
+  return (
+    <div className="grid-item popup-window">
+      <h2>
+        {this.props.modify ? "Modify task" : "Add a task"}
+      </h2>
+      <form>
+        <div className="form-grid">
+          <div className="form-field task-name">
+            <label htmlFor="name">
+              Name task <span className="red">*</span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={props.name}
+            />
           </div>
-          <p>
-            <span className="red">*</span> mandatory field
-          </p>
-          <input
-            className="submit-button"
-            type="submit"
-            value={
-              this.props.modify
-                ? "Save changes"
-                : "Add task"
-            }
-          />
-        </form>
-      </div>
-    );
-  }
-}
+          <div className="form-field task-category">
+            <label htmlFor="category">
+              Category <span className="red">*</span>
+            </label>
+            <select
+              id="category"
+              name="category"
+            >
+              <option
+                value="School"
+                selected={props.category == "School"}
+              >
+                School
+              </option>
+              <option
+                value="Social"
+                selected={props.category == "Social"}
+              >
+                Social
+              </option>
+              <option
+                value="Home"
+                selected={props.category == "Home"}
+              >
+                Home
+              </option>
+              <option
+                value="Misc"
+                selected={props.category == "Misc"}
+              >
+                Misc
+              </option>
+            </select>
+          </div>
+          <div className="form-field task-deadline-date">
+            <label htmlFor="deadline_date">
+              Deadline date
+            </label>
+            <input
+              type="date"
+              id="deadline_date"
+              name="deadline_date"
+              value={props.deadline_date}
+            />
+          </div>
+          <div className="form-field task-deadline-time">
+            <label htmlFor="deadline_time">
+              Deadline time
+            </label>
+            <input
+              type="time"
+              id="deadline_time"
+              name="deadline_time"
+              value={props.deadline_time}
+            />
+          </div>
+        </div>
+        <p>
+          <span className="red">*</span> mandatory field
+        </p>
+        <input
+          className="submit-button"
+          type="submit"
+          value={props.modify ? "Save changes" : "Add task"}
+        />
+      </form>
+    </div>
+  );
+};
+
 export default ModifyTaskMenu;
