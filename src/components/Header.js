bfcda540da99c0462f4fe74af14374a1b0c47741
MennoMuller <menno.muller.nl@gmail.com>
2023-01-26 14:11:41 +0100

    turned Header into a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-class Header extends React.Component {
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.props.tick(),
-      500
-    );
-  }
+// class Header extends React.Component {
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.props.tick(),
+//       500
+//     );
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
 
-  getDate = (date) => {
+//   getDate = (date) => {
+//     return (
+//       date.getDate() +
+//       "/" +
+//       (date.getMonth() + 1) +
+//       "/" +
+//       date.getFullYear()
+//     );
+//   };
+
+//   getTime = (date) => {
+//     return (
+//       date.getHours() +
+//       ":" +
+//       (date.getMinutes() >= 10
+//         ? date.getMinutes()
+//         : "0" + date.getMinutes()) +
+//       ":" +
+//       (date.getSeconds() >= 10
+//         ? date.getSeconds()
+//         : "0" + date.getSeconds())
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <header>
+//         <span className="date">
+//           {this.getDate(this.props.date)}
+//         </span>
+//         <span className="time">
+//           {this.getTime(this.props.date)}
+//         </span>
+//       </header>
+//     );
+//   }
+// }
+
+const Header = (props) => {
+  let timerID;
+
+  useEffect(() => {
+    timerID = setInterval(() => props.tick(), 500);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  const getDate = (date) => {
     return (
       date.getDate() +
       "/" +
@@ -22,7 +70,7 @@ class Header extends React.Component {
     );
   };
 
-  getTime = (date) => {
+  const getTime = (date) => {
     return (
       date.getHours() +
       ":" +
@@ -36,18 +84,12 @@ class Header extends React.Component {
     );
   };
 
-  render() {
-    return (
-      <header>
-        <span className="date">
-          {this.getDate(this.props.date)}
-        </span>
-        <span className="time">
-          {this.getTime(this.props.date)}
-        </span>
-      </header>
-    );
-  }
-}
+  return (
+    <header>
+      <span className="date">{getDate(props.date)}</span>
+      <span className="time">{getTime(props.date)}</span>
+    </header>
+  );
+};
 
 export default Header;
