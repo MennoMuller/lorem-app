7ab6e530355ad19c285bd1e74cd5117828996a60
MennoMuller <menno.muller.nl@gmail.com>
2023-01-26 12:52:25 +0100

    Componentized sidebar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import dashboardIcon from "./img/dashboard.png";
 import tasksIcon from "./img/tasks.png";
 import websitesIcon from "./img/websites.png";
 import weatherIcon from "./img/weather.png";
+import Sidebar from "./components/Sidebar";
 
 class App extends React.Component {
   state = {
@@ -207,61 +208,10 @@ class App extends React.Component {
           date={this.state.date}
           tick={this.tick}
         />
-        <div className="sidebar">
-          <div className="sidebar-header">
-            <Logo />
-            <Avatar
-              image={avatar}
-              username={this.state.user.username}
-            />
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/">
-                  <img
-                    src={dashboardIcon}
-                    alt=""
-                    className="nav-icon"
-                  />
-                  <span className="nav-text">
-                    Dashboard
-                  </span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/tasks">
-                  <img
-                    src={tasksIcon}
-                    alt=""
-                    className="nav-icon"
-                  />
-                  <span className="nav-text">Tasks</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/websites">
-                  <img
-                    src={websitesIcon}
-                    alt=""
-                    className="nav-icon"
-                  />
-                  <span className="nav-text">Websites</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/weather">
-                  <img
-                    src={weatherIcon}
-                    alt=""
-                    className="nav-icon"
-                  />
-                  <span className="nav-text">Weather</span>
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <Sidebar
+          avatar={avatar}
+          username={this.state.user.username}
+        />
         <Routes>
           <Route
             path="/"
